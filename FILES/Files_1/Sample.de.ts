9e3ea16651681d7d0bc8ed08e1889b8426daf8a8
Marius Santini <santinimarius@gmail.com>
2016-11-10 01:52:25 +0100

    LingoHub Update :rocket:
    Manual push by LingoHub User: Marius Santini.
    Project: GITHUB_REPOSITORY
    
    Made with :heart: by https://lingohub.com

---
+++ FILES/Files_1/Sample.de.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS language="de" version="2.0">
+  <context>
+    <name>kernel/navigationpart</name>
+    <message>
+      <source>Newsletter</source>
+      <translation>Rundschreiben</translation>
+    </message>
+    <message>
+      <source>vztnewsletter</source>
+      <translation/>
+      <comment>Navigation part</comment>
+    </message>
+    <message>
+      <source>%1 takes at most %n argument(s). %2 is therefore invalid.</source>
+      <translation>%1nimmt höchstens% n Argument(e). %2ist daher ungültig.</translation>
+    </message>
+  </context>
+  <context>
+    <message>
+      <source>%1 e-mail(s) bounced back</source>
+      <translation>%1E-Mail (s) prallte zurück</translation>
+    </message>
+    <message>
+      <source>Links clicked</source>
+      <translation>Links geklickt</translation>
+    </message>
+    <message>
+      <source>This link is clicked</source>
+      <translation>Dieser Link geklickt wird</translation>
+    </message>
+    <message>
+      <source>times</source>
+      <translation>mal</translation>
+    </message>
+  </context>
+</TS>
